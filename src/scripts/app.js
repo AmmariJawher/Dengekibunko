c7a7cb3e2675e7b5d2b9b48aeb290c82514c1081
AmmariJawher <jawher.ammari@tutanota.com>
2019-05-17 10:03:39 +0100

    Fix front-end architecture

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,21 +3,22 @@ var images = [];
 var time = 3000;
 
 // Image List
-images[0] = "img/silde0.png";
-images[1] = "img/silde1.png";
-images[2] = "img/silde2.png";
+images[0] = "/dist/img/slide0.png";
+images[1] = "/dist/img/slide1.jpg";
+images[2] = "/dist/img/slide2.png";
 
-// Change Image
+
 function changeImg(){
-    document.slide.src  = images[i];
+    console.log(images[i]);
+    document.getElementById("slide-img").src = images[i];
 
     if (i < images.length -1){
         i++;
     } else {
         i = 0;
     }
-
-    setTimeout("changeImg()", Time);
+    setTimeout("changeImg()", time);
+    console.log("end");
 }
 
 window.onload = changeImg;
