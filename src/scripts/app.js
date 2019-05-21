19feab46d2c9140c3c60cb807affa4c30a1f4172
AmmariJawher <jawher.ammari@tutanota.com>
2019-05-21 13:47:05 +0100

    Add navigation list for main slider
    Allow user to navigate through the slider not only forth and back but
    also by choosing the the slide from the main slider nav list. style has
    been add it and some light fix has been done.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
-var i = 0;
+/* var i = 0;
 var images = [];
 var time = 3000;
 
@@ -21,4 +21,79 @@ function changeImg(){
     console.log("end");
 }
 
-window.onload = changeImg;
+window.onload = changeImg; */
+
+
+
+
+
+/* MAIN SLIDER */
+
+const mainSliderList = document.querySelector(".main-slider-list");
+const mainSliderItem = document.querySelectorAll(".main-slider-item");
+
+//Buttons: Navigate forth and back in the slider
+const prevBtn = document.querySelector("#main-slider-controls-prev")
+const nextBtn = document.querySelector("#main-slider-controls-next")
+
+//Counters: Keep 
+let counter = 1;
+let navDotCounter;
+const size = mainSliderItem[0].clientWidth;
+
+mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+
+//Slider Navigation Dots: Navigete through the slider items
+const navDot = document.querySelectorAll(".nav-dot");
+
+
+//Buttons Listener: Make sure counter value between 1 and 5, animation add it, counter plus 1 or minus 1 and slider will change.
+nextBtn.addEventListener("click", () => {
+    if (counter >= mainSliderList.childElementCount - 1) return;
+    mainSliderList.style.transition = "transform 0.2s ease-out";
+    counter++;
+    mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+});
+prevBtn.addEventListener("click", () => {
+    if (counter <= 0) return;
+    mainSliderList.style.transition = "transform 0.2s ease-in-out";
+    counter--;
+    mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+});
+
+//Main Slider Listener: Make sure to keep the slide in an infinite loop by changing the slide to the original slide instantly without the user notice.
+mainSliderList.addEventListener("transitionend", () => {
+    if (mainSliderItem[counter].id === "lastClone") {
+        mainSliderList.style.transition = "none";
+        counter = mainSliderList.childElementCount - 2;
+        mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+    };
+
+    if (mainSliderItem[counter].id === "firstClone") {
+        mainSliderList.style.transition = "none";
+        counter = mainSliderList.childElementCount - 6;
+        mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+    };
+});
+
+
+//Dot navigation list Listener: 
+navDot.forEach(function (check) {
+    check.addEventListener('click', checkIndex);
+})
+
+function checkIndex(event) {
+    navDotCounter = (Array.from(navDot).indexOf(event.target));
+    counter = navDotCounter + 1;
+    mainSliderList.style.transition = "transform 0.2s ease-in-out";
+    mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+}
+
+mainSliderList.addEventListener("transitionend", () => {
+    for (index = 0; index < navDot.length; index++) {
+        navDot[index].style.opacity = "0.6";
+    }
+    navDotCounter = counter - 1;
+    navDot[navDotCounter].style.opacity = "1";
+
+});
