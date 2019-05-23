71c4ce489bd8d85682933b58ceb3b9bad50e1a9c
AmmariJawher <jawher.ammari@tutanota.com>
2019-05-23 13:30:59 +0100

    Fix light issues

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,64 +1,55 @@
-/* var i = 0;
-var images = [];
-var time = 3000;
-
-// Image List
-images[0] = "/dist/img/slide0.png";
-images[1] = "/dist/img/slide1.jpg";
-images[2] = "/dist/img/slide2.png";
-
-
-function changeImg(){
-    console.log(images[i]);
-    document.getElementById("slide-img").src = images[i];
-
-    if (i < images.length -1){
-        i++;
-    } else {
-        i = 0;
-    }
-    setTimeout("changeImg()", time);
-    console.log("end");
-}
-
-window.onload = changeImg; */
-
-
-
-
-
+//TODO: Fix slider.
 /* MAIN SLIDER */
 
 const mainSliderList = document.querySelector(".main-slider-list");
 const mainSliderItem = document.querySelectorAll(".main-slider-item");
 
 //Buttons: Navigate forth and back in the slider
-const prevBtn = document.querySelector("#main-slider-controls-prev")
-const nextBtn = document.querySelector("#main-slider-controls-next")
+const prevBtn = document.querySelector("#main-slider-controls-prev");
+const nextBtn = document.querySelector("#main-slider-controls-next");
 
 //Counters: Keep 
 let counter = 1;
-let navDotCounter;
 const size = mainSliderItem[0].clientWidth;
 
-mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+
+mainSliderList.style.transform = "translateX(-840px)";
+console.log(counter);
+
 
 //Slider Navigation Dots: Navigete through the slider items
 const navDot = document.querySelectorAll(".nav-dot");
 
+function moveSlide(argument) {
+    mainSliderList.style.transition = "transform 0.2s ease-out";
+    if (argument === true) {
+        counter++
+    } else if (argument === false) {
+        counter--;
+    }
+    mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+}
 
+//TODO: Automate the slider.
+function autoNextSlide() {
+    setInterval(moveSlide(true), 3000);
+}
+document.onload = autoNextSlide;
 //Buttons Listener: Make sure counter value between 1 and 5, animation add it, counter plus 1 or minus 1 and slider will change.
 nextBtn.addEventListener("click", () => {
-    if (counter >= mainSliderList.childElementCount - 1) return;
-    mainSliderList.style.transition = "transform 0.2s ease-out";
-    counter++;
-    mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+    if (counter === 6) {
+        return;
+    };
+    console.log(counter);
+    moveSlide(true);
+    console.log(counter);
+
 });
 prevBtn.addEventListener("click", () => {
-    if (counter <= 0) return;
-    mainSliderList.style.transition = "transform 0.2s ease-in-out";
-    counter--;
-    mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+    if (counter === 0) {
+        return;
+    };
+    moveSlide(false);
 });
 
 //Main Slider Listener: Make sure to keep the slide in an infinite loop by changing the slide to the original slide instantly without the user notice.
@@ -73,27 +64,22 @@ mainSliderList.addEventListener("transitionend", () => {
         mainSliderList.style.transition = "none";
         counter = mainSliderList.childElementCount - 6;
         mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
-    };
-});
-
-
-//Dot navigation list Listener: 
-navDot.forEach(function (check) {
-    check.addEventListener('click', checkIndex);
-})
-
-function checkIndex(event) {
-    navDotCounter = (Array.from(navDot).indexOf(event.target));
-    counter = navDotCounter + 1;
-    mainSliderList.style.transition = "transform 0.2s ease-in-out";
-    mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
-}
-
-mainSliderList.addEventListener("transitionend", () => {
-    for (index = 0; index < navDot.length; index++) {
+    }
+    
+    ///Any changes to the Slider will affect the Navigation List style
+    for (let index = 0; index < navDot.length; index++) {
         navDot[index].style.opacity = "0.6";
     }
-    navDotCounter = counter - 1;
-    navDot[navDotCounter].style.opacity = "1";
+    navDot[counter].style.opacity = "1";
+});
 
+//Control Slider from the Navigation List.
+function checkIndex(event) {
+    counter = (Array.from(navDot).indexOf(event.target));
+    mainSliderList.style.transition = "transform 0.2s ease-in-out";
+    mainSliderList.style.transform = "translateX(" + (-size * counter) + "px)";
+};
+
+navDot.forEach(function (check) {
+    check.addEventListener('click', checkIndex);
 });
