85c5add2248cd5ef1ef3659920e23125d4517a23
AmmariJawher <jawher.ammari@tutanota.com>
2019-06-02 14:15:01 +0100

    Add Uglify and fix style and script reload
    Using uglify to compress js files and fix html img/script/style src typo
    problem.

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,13 @@
 const gulp = require('gulp');
 
 // Include Our Plugins
-const sass = require('gulp-sass');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var uglifyjs = require('uglify-es');
+var composer = require('gulp-uglify/composer');
+var pump = require('pump');
+
+var minify = composer(uglifyjs, console);
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
@@ -22,11 +27,25 @@ gulp.task('browser-sync', function() {
   });
 });
 
+// Minify JavaScript with UglifyJS3.
+gulp.task('handleJs', function (cb) {
+  var options = {};
+
+  pump([
+      gulp.src('src/scripts/app.js'),
+      minify(options),
+      gulp.dest('dist/scripts')
+    ],
+    cb
+  );
+});
+
 // Watch Sass & Serve
 gulp.task('default', function() {
   browserSync.init({
     server: "./dist"
   });
   gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('src/scripts/app.js', gulp.series('handleJs'));
   gulp.watch("dist/**/*").on('change', browserSync.reload);
 });
